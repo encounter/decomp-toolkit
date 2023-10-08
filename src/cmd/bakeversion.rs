6cda4c88d1879124b48b772fbf3b39bfacd8d7e6
Phillip Stephens <antidote.crk@gmail.com>
2023-10-08 13:55:34 -0700

    Make metroidbuildinfo generic and rename to bakeversion
    Renames metriodbuildinfo to bakeversion, and adds two arguments
    `build_tag` and `max_len`.
    
    The argument `max_len` defaults to 35 while build_tag is positional and required
    
    This makes metroidbuildinfo useable by any project that wants to de-hardcode
    baked version strings with minimal effort

--- src/cmd/bakeversion.rs
+++ src/cmd/bakeversion.rs
@@ -7,18 +7,21 @@ use memmap2::MmapOptions;
 
 #[derive(FromArgs, PartialEq, Eq, Debug)]
 /// Sets the MetroidBuildInfo tag value in a given binary.
-#[argp(subcommand, name = "metroidbuildinfo")]
+#[argp(subcommand, name = "bakeversion")]
 pub struct Args {
     #[argp(positional)]
     /// path to source binary
     binary: PathBuf,
     #[argp(positional)]
+    /// build tag marker to search for
+    build_tag: String,
+    #[argp(positional)]
     /// path to build info string
     build_info: PathBuf,
+    #[argp(option, short = 'm', default="35")]
+    /// the max length of the version string, excluding build_tag (default: 35)
+    max_len: usize,
 }
-
-const BUILD_STRING_MAX: usize = 35;
-const BUILD_STRING_TAG: &str = "!#$MetroidBuildInfo!#$";
 
 pub fn run(args: Args) -> Result<()> {
     let build_string = std::fs::read_to_string(&args.build_info).with_context(|| {
@@ -26,9 +29,11 @@ pub fn run(args: Args) -> Result<()> {
     })?;
     let build_string_trim = build_string.trim_end();
     let build_string_bytes = build_string_trim.as_bytes();
+
+    let max_len = args.max_len;
     ensure!(
-        build_string_bytes.len() <= BUILD_STRING_MAX,
-        "Build string '{build_string_trim}' is greater than maximum size of {BUILD_STRING_MAX}"
+        build_string_bytes.len() <= max_len,
+        "Build string '{build_string_trim}' is greater than maximum size of {max_len}"
     );
 
     let binary_file =
@@ -37,8 +42,8 @@ pub fn run(args: Args) -> Result<()> {
         })?;
     let mut map = unsafe { MmapOptions::new().map_mut(&binary_file) }
         .with_context(|| format!("Failed to mmap binary: '{}'", args.binary.display()))?;
-    let start = match memmem::find(&map, BUILD_STRING_TAG.as_bytes()) {
-        Some(idx) => idx + BUILD_STRING_TAG.as_bytes().len(),
+    let start = match memmem::find(&map, args.build_tag.as_bytes()) {
+        Some(idx) => idx + args.build_tag.as_bytes().len(),
         None => bail!("Failed to find build string tag in binary"),
     };
     let end = start + build_string_bytes.len();
