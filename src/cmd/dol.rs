a94edb573f5c185990fc38818752496b31ae8403
Robin Avery <robin@ribbanya.dev>
2024-02-28 21:18:58 -0500

    Emit an error code when `dol diff` does not match (#43)
    * Emit an error code when `dol diff` does not match
    
    * `exit(1)` instead of `bail`

--- src/cmd/dol.rs
+++ src/cmd/dol.rs
@@ -1534,7 +1534,7 @@ fn diff(args: DiffArgs) -> Result<()> {
                     linked_sym.address,
                 );
             }
-            return Ok(());
+            std::process::exit(1);
         }
     }
 
@@ -1575,7 +1575,7 @@ fn diff(args: DiffArgs) -> Result<()> {
             );
             log::error!("Original: {}", hex::encode_upper(orig_data));
             log::error!("Linked:   {}", hex::encode_upper(linked_data));
-            return Ok(());
+            std::process::exit(1);
         }
     }
 
