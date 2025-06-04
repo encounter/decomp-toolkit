9d1561a45cd6da6cb266f31351ac68da59603325
Amber Brault <celestialamber1@gmail.com>
2025-06-04 00:17:32 -0700

    Simplify extab padding replacement

--- src/util/extab.rs
+++ src/util/extab.rs
@@ -27,97 +27,16 @@ pub fn clean_extab(obj: &mut ObjInfo, mut padding: impl Iterator<Item = u8>) -> 
         })?;
         let mut updated = false;
         for action in &decoded.exception_actions {
-            let section_offset = (symbol.address - extab_section.address) as usize
-                + action.action_offset as usize
-                + 2;
-            // TODO: does cwextab provide a way to serialize the action back to bytes?
-            let clean_data = match action.get_exaction_data() {
-                cwextab::ExActionData::DestroyLocalCond {
-                    condition,
-                    pad4: _,
-                    local_offset,
-                    dtor_address,
-                } => {
-                    let mut clean_data = Vec::new();
-                    clean_data.extend_from_slice(&condition.to_be_bytes());
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.extend_from_slice(&local_offset.to_be_bytes());
-                    clean_data.extend_from_slice(&dtor_address.to_be_bytes());
-                    Some(clean_data)
-                }
-                cwextab::ExActionData::DestroyMemberCond {
-                    condition,
-                    object_pointer,
-                    member_offset,
-                    pad8: _,
-                    dtor_address,
-                } => {
-                    let mut clean_data = Vec::new();
-                    clean_data.extend_from_slice(&condition.to_be_bytes());
-                    clean_data.extend_from_slice(&object_pointer.to_be_bytes());
-                    clean_data.extend_from_slice(&member_offset.to_be_bytes());
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.extend_from_slice(&dtor_address.to_be_bytes());
-                    Some(clean_data)
-                }
-                cwextab::ExActionData::DeletePointerCond {
-                    condition,
-                    object_pointer,
-                    pad4: _,
-                    dtor_address,
-                } => {
-                    let mut clean_data = Vec::new();
-                    clean_data.extend_from_slice(&condition.to_be_bytes());
-                    clean_data.extend_from_slice(&object_pointer.to_be_bytes());
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.extend_from_slice(&dtor_address.to_be_bytes());
-                    Some(clean_data)
-                }
-                cwextab::ExActionData::CatchBlock {
-                    pad0: _,
-                    catch_type,
-                    catch_pc_offset,
-                    cinfo_ref,
-                } => {
-                    let mut clean_data = Vec::new();
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.extend_from_slice(&catch_type.to_be_bytes());
-                    clean_data.extend_from_slice(&catch_pc_offset.to_be_bytes());
-                    clean_data.extend_from_slice(&cinfo_ref.to_be_bytes());
-                    Some(clean_data)
-                }
-                cwextab::ExActionData::CatchBlock32 {
-                    pad0: _,
-                    catch_type,
-                    catch_pc_offset,
-                    cinfo_ref,
-                } => {
-                    let mut clean_data = Vec::new();
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.push(padding.next().unwrap_or(0));
-                    clean_data.extend_from_slice(&catch_type.to_be_bytes());
-                    clean_data.extend_from_slice(&catch_pc_offset.to_be_bytes());
-                    clean_data.extend_from_slice(&cinfo_ref.to_be_bytes());
-                    Some(clean_data)
-                }
-                cwextab::ExActionData::EndOfList
-                | cwextab::ExActionData::Branch { .. }
-                | cwextab::ExActionData::DestroyLocal { .. }
-                | cwextab::ExActionData::DestroyLocalPointer { .. }
-                | cwextab::ExActionData::DestroyLocalArray { .. }
-                | cwextab::ExActionData::DestroyBase { .. }
-                | cwextab::ExActionData::DestroyMember { .. }
-                | cwextab::ExActionData::DestroyMemberArray { .. }
-                | cwextab::ExActionData::DeletePointer { .. }
-                | cwextab::ExActionData::ActiveCatchBlock { .. }
-                | cwextab::ExActionData::Terminate
-                | cwextab::ExActionData::Specification { .. } => None,
-            };
-            if let Some(clean_data) = clean_data {
+            // Check if the current action has padding
+            if let Some(padding_offset) = action.get_struct_padding_offset() {
+                let index = padding_offset as usize;
+                let section_offset = (symbol.address - extab_section.address) as usize
+                    + action.action_offset as usize;
+                let mut clean_data: Vec<u8> = action.get_exaction_bytes(false);
+                // Write the two padding bytes
+                clean_data[index + 0] = padding.next().unwrap_or(0);
+                clean_data[index + 1] = padding.next().unwrap_or(0);
+
                 let orig_data =
                     &mut extab_section.data[section_offset..section_offset + clean_data.len()];
                 orig_data.copy_from_slice(&clean_data);
