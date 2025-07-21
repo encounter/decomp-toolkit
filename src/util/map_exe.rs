39862c212ebc4435e7e5cae35df48cc092a485f3
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-07-21 12:03:35 -0700

    remove duplicate syms for imps

--- src/util/map_exe.rs
+++ src/util/map_exe.rs
@@ -1,4 +1,4 @@
-use std::collections::{btree_map, BTreeMap, HashMap};
+use std::collections::{btree_map, BTreeMap, BTreeSet, HashMap};
 use typed_path::Utf8NativePathBuf;
 use std::fs::read_to_string;
 use std::str::SplitWhitespace;
@@ -27,6 +27,7 @@ pub struct ExeSectionInfo {
     pub section_type: ExeSectionType,
 }
 
+#[derive(Clone)]
 pub struct ExeSymbolEntry {
     pub addr: SectionAddress,
     pub symbol: String,
@@ -37,9 +38,13 @@ pub struct ExeSymbolEntry {
 }
 
 pub struct ExeMapInfo {
-    pub entry_point: u32,
+    pub preferred_load_addr: u32,
+    // the different sections of the map
     pub sections: Vec<ExeSectionInfo>,
+    // the symbols found at each section of the map
     pub section_symbols: Vec<Vec<ExeSymbolEntry>>,
+    // the addresses in the map that have more than one symbol for them
+    pub merged_addrs: Vec<SectionAddress>,
 
     // pub entry_point: String,
     // pub unit_entries: MultiMap<String, SymbolRef>,
@@ -54,10 +59,10 @@ pub struct ExeMapInfo {
 
 impl ExeMapInfo {
     pub fn new() -> Self {
-        ExeMapInfo { entry_point: 0, sections: Vec::new(), section_symbols: Vec::new() }
+        ExeMapInfo { preferred_load_addr: 0, sections: Vec::new(), section_symbols: Vec::new(), merged_addrs: Vec::new() }
     }
 
-    fn set_entry_point(&mut self, entry_point: u32) { self.entry_point = entry_point; }
+    fn set_preferred_load_addr(&mut self, entry_point: u32) { self.preferred_load_addr = entry_point; }
 
     fn add_section(&mut self, section: ExeSectionInfo) {
         self.sections.push(section);
@@ -92,9 +97,78 @@ impl ExeMapInfo {
         });
         Ok(())
     }
+
+    pub fn resolve_merged_entries(&mut self) -> Result<()> {
+        fn check_for_imp_case(entries: Vec<&ExeSymbolEntry>) -> Option<String> {
+            // __imp_{name} and {name} case
+            if entries.len() == 2 {
+                let mut imp = None;
+                let mut thunk = None;
+
+                for dupe in entries {
+                    if dupe.symbol.starts_with("__imp_"){
+                        imp = Some(dupe.symbol.trim_start_matches("__imp_"));
+                    }
+                    else {
+                        thunk = Some(dupe.symbol.as_str());
+                    }
+                }
+
+                if let(Some(imp), Some(thunk)) = (imp, thunk) {
+                    if imp == thunk {
+                        // println!("Unnecessary thunk {} found!", thunk);
+                        return Some(thunk.to_string());
+                    }
+                }
+            }
+            return None;
+        }
+
+        for entries in self.section_symbols.iter_mut() {
+            let mut counts = HashMap::new();
+            for entry in entries.iter() {
+                *counts.entry(entry.addr).or_insert(0) += 1;
+            }
+
+            let mut dupe_map: MultiMap<SectionAddress, usize> = Default::default(); // store indices
+            for (idx, entry) in entries.iter().enumerate() {
+                if counts.get(&entry.addr).copied().unwrap_or(0) > 1 {
+                    dupe_map.insert(entry.addr, idx);
+                }
+            }
+
+            let mut symbols_to_remove: Vec<String> = Vec::new();
+            for (addr, indices) in dupe_map.iter_all() {
+                let group: Vec<&ExeSymbolEntry> = indices.iter().map(|&i| &entries[i]).collect();
+
+                // print!("{} has entries: ", addr);
+                // for dupe in &group {
+                //     print!("{} ", dupe.symbol);
+                // }
+                // println!();
+
+                // resolve imp_Blahs and Blahs that are at the same address
+                if let Some(thunk_sym) = check_for_imp_case(group) {
+                    symbols_to_remove.push(thunk_sym.to_string());
+                }
+                else {
+                    // add this addr to a list of merged addrs
+                    self.merged_addrs.push(*addr);
+                }
+            }
+
+            entries.retain(|e| !symbols_to_remove.contains(&e.symbol));
+        }
+
+        // do something with code merged symbols/units
+        // maybe: for an address that has a bunch of code merged symbols, look at the prev and next addrs and see what their units are
+        // then wipe out any code merged entry that doesn't belong to those aforementioned units
+        Ok(())
+    }
+
 }
 
-pub const ENTRY_STR: &str = " Preferred load address is ";
+pub const PREFERRED_LOAD_ADDR_STR: &str = " Preferred load address is ";
 pub const SECTION_STR: &str = " Start         Length     Name                   Class";
 pub const ADDR_STR: &str = "  Address         Publics by Value              Rva+Base       Lib:Object";
 pub const STATIC_SYM_STR: &str = " Static symbols";
@@ -114,10 +188,10 @@ pub fn process_map_exe(map_path: &Utf8NativePathBuf) -> Result<ExeMapInfo> {
     let mut must_read_syms = true;
 
     for line in read_to_string(map_path)?.lines() {
-        if line.contains(ENTRY_STR) {
-            let entry_str = line.split(ENTRY_STR).collect::<Vec<&str>>();
+        if line.contains(PREFERRED_LOAD_ADDR_STR) {
+            let entry_str = line.split(PREFERRED_LOAD_ADDR_STR).collect::<Vec<&str>>();
             assert_eq!(entry_str.len(), 2);
-            exe_map_info.set_entry_point(u32::from_str_radix(&entry_str[1].clone(), 16)?);
+            exe_map_info.set_preferred_load_addr(u32::from_str_radix(&entry_str[1].clone(), 16)?);
         }
         else if line == SECTION_STR {
             state = ExeMapState::ReadingSections;
@@ -183,15 +257,10 @@ pub fn process_map_exe(map_path: &Utf8NativePathBuf) -> Result<ExeMapInfo> {
                     }
                 }
                 let symbol_parts = line.split_whitespace().collect::<Vec<&str>>();
-                if symbol_parts[1] == "??__Eg_RGBScaledToYUV601@D3D@@YAXXZ" {
-                    println!("here");
-                }
-                // for (idx, part) in symbol_parts.iter().enumerate() {
-                //     println!("{}: {}", idx, part);
-                // }
                 exe_map_info.add_symbol(symbol_parts, true)?;
             }
         }
     }
+    exe_map_info.resolve_merged_entries()?;
     Ok(exe_map_info)
 }
